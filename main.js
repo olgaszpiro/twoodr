5eb387f7536143da5da8975e12eab69501d4b58d
Olga Szpiro <olgaszpiro@Olgas-MacBook-Pro.local>
2021-01-28 15:13:54 -0800

    Olga and Louisa

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
-document.querySelector('#submit-twood').addEventListener('click', () => {
-  const newText = document.querySelector('#twood-input').value
-  // document.querySelector('.twood').innerText = newText
-  document.querySelector('#twood-input').value = ''
+ document.querySelector('#submit-twood').addEventListener('click', 
+ function(){
+ const newTwood = document.querySelector('#twood-input').value
+ document.querySelector('.twood').innerText = newTwood
+ console.log(newTwood)
+ document.querySelector('#twood-input').value = " "
+ })
 
-  const newTwood = document.createElement('div')
-  document.querySelector('.twoods-container').append(newTwood)
-  // newTwood.classList.add('twood')
-  // newTwood.innerText = newText
+document.querySelector('#submit-twood').addEventListener('click',
+function(){
+console.log("The button was clicked")
+}
+)
 
 
-})
 
-setTimeout(() => {
-  document.querySelector('.cutoff').classList.remove('hidden')  
-}, 200000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('#submit-twood').addEventListener('click', () => {
+//   const newText = document.querySelector('#twood-input').value
+//   document.querySelector('.twood').innerText = newText
+//   document.querySelector('#twood-input').value = ''
+
+//   const newTwood = document.createElement('div')
+//   document.querySelector('.twoods-container').append(newTwood)
+//   // newTwood.classList.add('twood')
+//   // newTwood.innerText = newText
+
+
+// })
+
+// setTimeout(() => {
+//   document.querySelector('.cutoff').classList.remove('hidden')  
+// }, 200000)
